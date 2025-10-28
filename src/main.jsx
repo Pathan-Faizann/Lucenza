d82e545677f1e8b81786e5b59e7b76fefb7cd4cf
Pathan-Faizan <faizanpathan8104@gmail.com>
2025-10-28 23:01:46 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { store } from './redux/store.js'
 import { Provider } from 'react-redux'
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -12,9 +12,9 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 createRoot(document.getElementById('root')).render(
  
     <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
     </Provider>
 
 )
