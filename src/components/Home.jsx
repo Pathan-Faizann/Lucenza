49aa99917b6b98a1c259477981405f2b10902954
Pathan-Faizan <faizanpathan8104@gmail.com>
2025-10-28 23:05:24 +0530

    deploy

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
       <div className='row  cont justify-content-around align-items-center'>
         <div className='col-6   animate__animated animate__fadeInLeft animate__slow  '><h2 className='text-dark display-4 text-center h2'>From streets to spotlight, <br /> every stitch we make is built <br /> for those who dare to stand out.</h2></div>
         <div className='col-6 part-2 animate__animated animate__fadeInRight animate__slow'>
-          <img className='img-fluid img ' src="/Fhero.jpg" alt="" />
+          <img className='img-fluid img ' src="/Lucenza/Fhero.jpg" alt="" />
           <img className='img-fluid anime animate__animated animate__fadeInDown animate__slower animate__delay-1s ' src="/aniFin.png" alt="" />
           </div>
       </div>
